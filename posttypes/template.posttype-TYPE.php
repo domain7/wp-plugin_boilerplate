aad44028ef84065578135e08cff410b14f111a4f
Nathan Shubert-Harbison <nathan@domain7.com>
2014-11-21 10:52:43 -0800

    Added better defaults for custom post type labels, using dynamic constant naming so constants only need to be written once

--- posttypes/template.posttype-TYPE.php
+++ posttypes/template.posttype-TYPE.php
@@ -17,10 +17,23 @@ add_action('init', 'posttype_register_TYPENAME', 0);
 function posttype_register_TYPENAME() { // The perameters of your custom post type.
 	
 	// Labels for your post type. Note that many more are available, as detailed in the codex.
+	$singular = 'TYPENAME (SINGULAR)';
+	$plural = 'TYPENAME (PLURAL)';
+
 	$labels = array(
-		'name' => _x('TYPENAME', 'post type general name'),
-		'singular_name' => _x('TYPENAME', 'post type singular name')
+		'name' => _x($plural, 'post type general name'),
+		'singular_name' => _x($singular, 'post type singular name'),
+		'add_new' => _x('Add New', $singular),
+		'add_new_item' => __('Add New ' . $singular),
+		'edit_item' => __('Edit ' . $singular),
+		'new_item' => __('New ' . $singular),
+		'view_item' => __('View ' . $singular),
+		'search_items' => __('Search ' . $plural),
+		'not_found' =>  __('no ' . strtolower($plural) . ' found'),
+		'not_found_in_trash' => __('No ' . strtolower($plural) . ' in Trash'),
+		'parent_item_colon' => ''
 	);
+
  
 	// For all available arguments go to http://codex.wordpress.org/Function_Reference/register_post_type
 	$args = array(
@@ -31,7 +44,7 @@ function posttype_register_TYPENAME() { // The perameters of your custom post ty
 		'query_var' => true,
 		'rewrite' => true,
 		'capability_type' => 'post',
-		'taxonomies' => array('category'), // Uses the custom taxonomy created in this template
+		//'taxonomies' => array('category'), // Uses the custom taxonomy created in this template
 		'has_archive' => true,
 		'supports' => array('title','editor','thumbnail')
 	  ); 
--- site_boilerplate.php
+++ site_boilerplate.php
@@ -12,13 +12,29 @@ Author URI: http://domain7.com
 Text Domain: sitename
 */
 
-define( 'SITENAME_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+/**
+ * Site name to creating constants.
+ * 
+ * Specify an uppercase sitename to be used as the base for constants such as directories.
+ * This is the only value you must edit in this file.
+ */
+
+$site = 'SITENAME';
+
+
+// Creates a constant name for the plugin directory.
+$plugin_dir_constant_name = strtoupper($site) . '_PLUGIN_DIR';
+
+
+// Creates the actual site plugin directory constant
+define($plugin_dir_constant_name, plugin_dir_path( __FILE__ ));
 
 
 /**
  * Custom post types
  */
-foreach (glob(SITENAME_PLUGIN_DIR . "posttypes/posttype-*.php") as $filename) {
+foreach (glob(constant($plugin_dir_constant_name) . "posttypes/posttype-*.php") as $filename) {
 	require_once($filename);
 }
 
@@ -26,7 +42,7 @@ foreach (glob(SITENAME_PLUGIN_DIR . "posttypes/posttype-*.php") as $filename) {
 /**
  * Custom taxonomies
  */
-foreach (glob(SITENAME_PLUGIN_DIR . "taxonomies/taxonomy-*.php") as $filename) {
+foreach (glob(constant($plugin_dir_constant_name) . "taxonomies/taxonomy-*.php") as $filename) {
 	require_once($filename);
 }
 
@@ -36,4 +52,4 @@ foreach (glob(SITENAME_PLUGIN_DIR . "taxonomies/taxonomy-*.php") as $filename) {
  */
 
 // If we're using ACF and JSON-REST-API, add custom fields to the API.
-require_once(SITENAME_PLUGIN_DIR . "includes/acf-json-api.php");
+require_once(constant($plugin_dir_constant_name) . "includes/acf-json-api.php");
